e7cd616653c433890c6d96e31b855a55b7fe241a
zhiruoyu <convert@163.com>
2016-07-09 09:29:23 +0800

    nihao

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,7 @@ var handlebars = require('express3-handlebars')
             .create({defaultLayout:'main'});
     app.engine('handlebars',handlebars.engine);
     app.set('view engine','handlebars');
-app.set('port',process.env.PORT || 80);
+app.set('port',process.env.PORT || 3000);
 app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res){
     res.type('text/html');
