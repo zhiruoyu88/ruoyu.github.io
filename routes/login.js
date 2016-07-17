12539df86df4d618a0108b74bf04e20ca845bfeb
zhiruoyu <convert@163.com>
2016-07-17 22:48:27 +0800

    jingtai

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express();
 app.get('/login',function(req,res){
-    res.send(888);
+    // res.writeHead(200,{'content-Type':'text/html'});
+    res.render('login');
 });
-moodule.exports = app;
+module.exports = app;
