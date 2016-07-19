d58233531238a929fd0a90d6f095beb60092f598
zhiruoyu <convert@163.com>
2016-07-20 07:53:47 +0800

    登录

--- meadowlark.js
+++ meadowlark.js
@@ -7,7 +7,7 @@ var handlebars = require('express3-handlebars')
     app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 8080);
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
