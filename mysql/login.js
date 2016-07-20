3472604eb9aa9cbfd6886a7b7b0a587bdb4564c8
zhiruoyu <convert@163.com>
2016-07-20 21:24:56 +0800

    登录

--- mysql/login.js
+++ mysql/login.js
@@ -12,19 +12,19 @@ function selectUsr (client,username,callback){
     client.query(
     'SELECT password FROM usr where name="'+admin+'"',
     function selectdb (err,results,fields){
-        // if(err){
-        //     throw err;
-        // }
-        // if(results)
-        // {
+        if(err){
+            throw err;
+        }
+        if(results)
+        {
             callback(results);
-        // }    
+        }    
     });
 }
 // 插入数据
 function insertSome (client , username , password,callback){
     client.query('insert into usr value(?,?)', [username, password], function(err,result){
-27         if( err ){
+27         if(err){
 28             console.log( "error:" + err.message);
 29             return err;
 30         }
