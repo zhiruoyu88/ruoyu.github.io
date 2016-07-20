35a3152b01771a5140468dc6cef766d0811d33de
zhiruoyu <convert@163.com>
2016-07-20 08:10:58 +0800

    登录

--- meadowlark.js
+++ meadowlark.js
@@ -7,7 +7,7 @@ var handlebars = require('express3-handlebars')
     app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 8080);
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json({ type: application/vnd.api+json }))
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(function (req, res, next) {
 next()
 });
