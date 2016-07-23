346d11f01cd1aec8411ea35ff9b44c9c731f8abf
zhiruoyu <convert@163.com>
2016-07-24 07:46:03 +0800

    sql

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express'),
     app = express(),
     select = require('../mysql/login');
 select.selectArticle(select.client,function(results){
-    console.log(results);
-    console.log(fields);
+    app.get('/',function(req,res){
+        console.log(results);
+        res.type('text/html');
+        res.render('home');
+    });
 })
-app.get('/',function(req,res){
-    res.type('text/html');
-    res.render('home');
-});
+
 module.exports = app;
