3b5078602c6790855e503a400a3f48b0019cf7a5
zhiruoyu <convert@163.com>
2016-07-24 21:41:47 +0800

    sql

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,16 @@ select.selectArticle(select.client,function(results){
             results={}
         }else{
             for(var i=0;i<results.length;i++){
+                var date = results[i].write_date,
+                year = date.getFullYear().toString(),
+                month = date.getMonth()+1,
+                day = date.getDate();
+                month = month<10?'0'+month:month;
+                results[i].write_date = year+'-'+month+'-'+day;
                 if(results[i].blog_tag==0){
-                    results.blog_tag='生活';
+                    results[i].blog_tag='生活';
                 }else if(results[i].blog_tag==1){
-                    results.blog_tag='技术';
+                    results[i].blog_tag='技术';
                 }
             }
             
