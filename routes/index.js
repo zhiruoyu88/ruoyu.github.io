07174fe63d435308e775cc646cb1300c95c77443
zhiruoyu <convert@163.com>
2016-07-24 23:00:55 +0800

    sql

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 var express = require('express'),
     app = express(),
     select = require('../mysql/login');
-Date.prototype.Format = function (fmt) { //author: meizz 
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+var format = function(time,format){
+    var t = new Date(time);
+    var tf = function(i){return (i < 10 ? '0' :'') + i};
+    return format.replace(/yyyy|MM|dd|HH|mm|ss/g,function(a){
+    switch(a){
+    case 'yyyy':
+    return tf(t.getFullYear());
+    break;
+    case 'MM':
+    return tf(t.getMonth() + 1);
+    break;
+    case 'mm':
+    return tf(t.getMinutes());
+    break;
+    case 'dd':
+    return tf(t.getDate());
+    break;
+    case 'HH':
+    return tf(t.getHours());
+    break;
+    case 'ss':
+    return tf(t.getSeconds());
+    break;
+    }
+    })
 }
 select.selectArticle(select.client,function(results){
     app.get('/',function(req,res){
@@ -27,7 +38,7 @@ select.selectArticle(select.client,function(results){
                 // month = date.getMonth()+1,
                 // day = date.getDate();
                 // month = month<10?'0'+month:month;
-                results[i].write_date = date.Format("yyyy-MM-dd");
+                results[i].write_date = format(date,'yyyy-MM-dd');
                 if(results[i].blog_tag==0){
                     results[i].blog_tag='生活';
                 }else if(results[i].blog_tag==1){
