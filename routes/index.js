4fbe4951b54e8ae78b296813f758741c3075ac52
zhiruoyu <convert@163.com>
2016-07-26 22:10:07 +0800

    sql

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,25 @@
 var express = require('express'),
     app = express(),
     select = require('../mysql/login');
-select.selectArticle(select.client,function(results){
+
     app.get('/',function(req,res){
-        if(!results){
-            results={}
-        }else{
-            for(var i=0;i<results.length;i++){
-                if(results[i].blog_tag==0){
-                    results[i].blog_tag='生活';
-                }else if(results[i].blog_tag==1){
-                    results[i].blog_tag='技术';
+        select.selectArticle(select.client,function(results){
+            if(!results){
+                results={}
+            }else{
+                for(var i=0;i<results.length;i++){
+                    if(results[i].blog_tag==0){
+                        results[i].blog_tag='生活';
+                    }else if(results[i].blog_tag==1){
+                        results[i].blog_tag='技术';
+                    }
                 }
+                
             }
-            
-        }
-        res.type('text/html');
-        res.render('home',{article:results});
+            res.type('text/html');
+            res.render('home',{article:results});
+        })
     });
-})
+
 
 module.exports = app;
